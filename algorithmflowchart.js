104b4546d587c382faaa2c3835ae0f3eea9b39d5
utkarsh18032 <utkarshpatil1834@gmail.com>
2022-11-02 16:48:58 +0530

    algo flowchart file added

---
+++ algorithmflowchart.js
@@ -0,0 +1,19 @@
+
+marks=37
+{
+    if(marks>90){
+        console.log("passed with A grade")
+    }
+    else if(marks>80){
+        console.log("passed with B grade")
+    }
+   else if(marks>60){
+    console.log("passed with C grade")
+   }
+   else if(marks>35){
+    console.log("passed with D grade")
+   }
+   else{
+    console.log("you are failed")
+   }
+}
